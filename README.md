d4e56b389384187ae74bd74e18d091c0beacc1c9
Ligiano Azzi <ldaazzi@gmail.com>
2019-02-04 22:41:25 +0000

    installing the project and setting the database

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# laravel-facilitating-development-with-php--course--alura" 
+
+composer create-project laravel/laravel estoque "5.0.*" --prefer-dist
+
---
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace estoque\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function lista(){
+    	return "<h1> Listagem de Produtos</h1>";
+    }
+}
